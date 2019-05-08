9d6049fd3f538d98507e856be06a44df0bb531e1
devhect <48111090+devhect@users.noreply.github.com>
2019-05-07 19:13:44 -0600

    Correcting comment formatting

--- 21-day-7-regular-expressions-ii.js
+++ 21-day-7-regular-expressions-ii.js
@@ -1,8 +1,8 @@
 /*
-* Declare a RegExp object variable named 're'
-* It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', followed by one or more letters
-* Do not remove the return statement
-*/
+ * Declare a RegExp object variable named 're'
+ * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', followed by one or more letters
+ * Do not remove the return statement
+ */
 function regexVar() {
     const re = /^(Mr?s|[MDE]r)\.[A-Za-z]+$/;
     return re;
