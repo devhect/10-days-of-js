b995c6da72c1ede17e6639d72bb557068d3b469b
devhect <48111090+devhect@users.noreply.github.com>
2019-05-08 11:21:57 -0600

    Adding 22-day-7-regular-expressions-iii.js

---
+++ 22-day-7-regular-expressions-iii.js
@@ -0,0 +1,9 @@
+/*
+ * Declare a RegExp object variable named 're'
+ * It must match ALL occurrences of numbers in a string.
+ * Do not remove the return statement
+ */
+function regexVar() {
+    let re = /\d+/g;
+    return re;
+};
